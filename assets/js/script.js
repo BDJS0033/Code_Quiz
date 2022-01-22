e882ffc4ea5afdfdbb61df57dd9509c7a8a649f8
SAB <sevanboyadjian@gmail.com>
2022-01-21 19:41:18 -0500

    WIP code

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// Link elements to the DOM
+var questions = document.getElementById("questions");
+var showAnswer = document.getElementById("answer");
+var start = document.getElementById("start");
+var sbutton = document.getElementById("sbutton");
+var quiz = document.getElementById("quiz");
+var scoreId = document.getElementById("score");
+var timer = document.querySelector(".timer");
+var choiceA = document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
+var choiceD = document.getElementById("D");
+var savebtn = document.getElementById("savebtn");
+var providedInitials = document.getElementById("providedInitials");
+
+//Quiz Questions
